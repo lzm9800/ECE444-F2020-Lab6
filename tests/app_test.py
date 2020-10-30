fd3e249ff496ce64a0b478d05ebde2554457febc
lzm9800 <43189674+lzm9800@users.noreply.github.com>
2020-10-30 17:34:06 -0400

    Delete must log in
    Added check for user to log in before can delete. Also added test to search.

--- tests/app_test.py
+++ tests/app_test.py
@@ -80,3 +80,18 @@ def test_delete_message(client):
     rv = client.get('/delete/1')
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+def test_search(client):
+    # make sure search exists
+    response = client.get("/search/?query = test", content_type="html/text")
+    assert response.status_code == 200
+
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 1
